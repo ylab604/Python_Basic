dc62de5831209ba5306011367ff667b2a445b25e
ylab604 <99255566+ylab604@users.noreply.github.com>
2022-02-08 20:41:54 +0900

    Add files via upload

---
+++ 혼공파/main.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass
+import test_module as test
+
+radius = test.number_input()
+print(test.get_circumference(radius))
+print(test.get_circle_area(radius))
+
+
+print(__name__)
